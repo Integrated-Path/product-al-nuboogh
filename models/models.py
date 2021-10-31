aeb048b2d72e3ae95685b10d147f092bd2325e5d
murtaja1 <murtajaadnan7@gmail.com>
2021-10-31 11:55:25 +0000

    first commit

---
+++ models/models.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class StockMoveLine(models.Model):
+    _inherit = 'stock.move.line'
+
+    partner_id = fields.Char('customer', compute="_Compute_partner_id")
+
+    @api.depends('origin')
+    def _Compute_partner_id(self):
+        for move in self:
+            if move.origin:
+                if move.origin[0] == 'S':
+                    move.partner_idP=  move.env['sale.order'].search([('name', '=', move.origin)]).partner_id.name
+                elif move.origin[0] == 'P':
+                    move.partner_id =  move.env['purchase.order'].search([('name', '=', move.origin)]).partner_id.name
+                else:
+                    move.partner_id=""
+            else: 
+                move.partner_id = ""
+        
+            
+    
