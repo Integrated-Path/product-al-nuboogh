635eeecf9f330ffe925e75e175b30a3f9cb3d1cf
murtaja1 <murtajaadnan7@gmail.com>
2021-11-22 09:43:32 +0000

    try to fix erro

--- models/models.py
+++ models/models.py
@@ -5,21 +5,20 @@ from odoo import models, fields, api
 class StockMoveLine(models.Model):
     _inherit = 'stock.move.line'
 
-    partner_id = fields.Char('customer', compute="_Compute_partner_id")
+    partner_id = fields.Char('customer')
 
-    @api.depends('origin')
-    def _Compute_partner_id(self):
-        for move in self:
-            for line in move:
-                if line.origin:
-                    if line.origin[0] == 'S':
-                        line.partner_id=  line.env['sale.order'].search([('name', '=', line.origin)]).partner_id.name
-                    elif line.origin[0] == 'P':
-                        line.partner_id =  line.env['purchase.order'].search([('name', '=', line.origin)]).partner_id.name
-                    else:
-                        line.partner_id=""
-                else: 
-                    line.partner_id = ""
+    # @api.depends('origin')
+    # def _Compute_partner_id(self):
+    #     for move in self:
+    #         if move.origin:
+    #             if move.origin[0] == 'S':
+    #                 move.partner_id=  move.env['sale.order'].search([('name', '=', move.origin)]).partner_id.name
+    #             elif move.origin[0] == 'P':
+    #                 move.partner_id =  move.env['purchase.order'].search([('name', '=', move.origin)]).partner_id.name
+    #             else:
+    #                 move.partner_id=""
+    #         else: 
+    #             move.partner_id = ""
         
             
     
