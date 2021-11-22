ca33e1f6f78b0b17fb1b9b62fbd6fba241b00136
murtaja1 <murtajaadnan7@gmail.com>
2021-11-22 12:17:10 +0000

    fix singletone error

--- models/models.py
+++ models/models.py
@@ -5,20 +5,9 @@ from odoo import models, fields, api
 class StockMoveLine(models.Model):
     _inherit = 'stock.move.line'
 
-    partner_id = fields.Char('customer')
+    partner_id = fields.Many2one('customer', related="move_id.partner_id",store=True)
 
-    # @api.depends('origin')
-    # def _Compute_partner_id(self):
-    #     for move in self:
-    #         if move.origin:
-    #             if move.origin[0] == 'S':
-    #                 move.partner_id=  move.env['sale.order'].search([('name', '=', move.origin)]).partner_id.name
-    #             elif move.origin[0] == 'P':
-    #                 move.partner_id =  move.env['purchase.order'].search([('name', '=', move.origin)]).partner_id.name
-    #             else:
-    #                 move.partner_id=""
-    #         else: 
-    #             move.partner_id = ""
+    
         
             
     
