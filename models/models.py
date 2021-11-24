3c8f3bd4b0be338c05dddf182990606c8d3922d3
murtaja <murtajaadnan7@gmail.com>
2021-11-24 13:45:34 +0300

    try fix error

--- models/models.py
+++ models/models.py
@@ -5,7 +5,7 @@ from odoo import models, fields, api
 class StockMoveLine(models.Model):
     _inherit = 'stock.move.line'
 
-    # partner_id = fields.Many2one(string='Customer', related="picking_id.partner_id")
+    partner_id = fields.Many2one(string='Customer', related="picking_id.partner_id")
 
     
         
