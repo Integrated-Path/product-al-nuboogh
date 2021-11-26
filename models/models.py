980efb45890a1cf0c9aad9f8fd46b38875bc3386
M.Saeb <48291783+M-Saeb@users.noreply.github.com>
2021-11-26 09:28:48 +0300

    Update models.py

--- models/models.py
+++ models/models.py
@@ -5,7 +5,7 @@ from odoo import models, fields, api
 class StockMoveLine(models.Model):
     _inherit = 'stock.move.line'
 
-    partner_id = fields.Many2one(string='Customer', related="picking_id.partner_id",store=True)
+    partner_id = fields.Many2one(string='Partner', related="picking_id.partner_id",store=True)
 
     
         
